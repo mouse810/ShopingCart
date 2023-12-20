17a305c7d37b5ed24ac330c29b5d738935ef38da
mouse810 <ytacc.mouse@gmail.com>
2023-12-20 11:11:14 +0530

    147

--- index.js
+++ index.js
@@ -28,23 +28,27 @@ let shopItemsData = [{
      disc: 'Cool Shirts for Man.Cool Shirts for Man.',
      img: 'images/img-4.jpg'
 },]
+// Basket
+
+const basket = []
 
 // Arrow function
 
 const generateShop = () => {
      return (shop.innerHTML = shopItemsData.map((x) => {
+          let { id, name, desc, price, img } = x
           return `
-     <div class="item">
+     <div id='product-id-${id}' class="item">
         <img width="220" src=${img} alt="" />
         <div class="details">
           <h3>${name}</h3>
-          <p>${disc}</p>
+          <p>${desc}</p>
           <div class="price-quantity">
             <h2>$ ${price}</h2>
             <div class="buttons">
-              <i class="bi bi-dash-lg"></i>
-              <div class="quantity">0</div>
-              <i class="bi bi-plus-lg"></i>
+              <i onclick='decrement(${id})' class="bi bi-dash-lg"></i>
+              <div id='${id}' class="quantity">0</div>
+              <i onclick='increment(${id})' class="bi bi-plus-lg"></i>
             </div>
           </div>
         </div>
@@ -54,3 +58,34 @@ const generateShop = () => {
 }
 
 generateShop()
+
+// Increment, Decrement and Update
+
+const increment = (id) => {
+     const selectedItem = id;
+     let search = basket.find((x) => x.id === selectedItem.id)
+     if (search === undefined) {
+          basket.push({
+               id: selectedItem.id,
+               item: 1
+          })
+     } else {
+          search.item += 1;
+     }
+     console.log(basket);
+}
+
+const decrement = (id) => {
+     const selectedItem = id;
+     let search = basket.find((x) => x.id === selectedItem.id)
+     if (search === undefined) {
+          basket.push({
+               id: selectedItem.id,
+               item: 1
+          })
+     } else {
+          search.item -= 1;
+     }
+     console.log(basket);
+}
+const update = () => { }
